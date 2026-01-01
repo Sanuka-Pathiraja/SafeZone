263da62b1b5cd78958b7db80c7be7017c55dbdb8
Navi2004725 <naveeshabeydeera123@gmail.com>
2026-01-02 00:58:55 +0530

    day 02 ✏️

---
+++ config/data-source.js
@@ -0,0 +1,16 @@
+import "dotenv/config";
+import { DataSource } from "typeorm";
+
+const AppDataSource = new DataSource({
+  type: "postgres",
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  synchronize: true,
+  logging: true,
+  entities: ["./Models/*.js"], // Adjust if folder name is different
+});
+
+export default AppDataSource;
--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 import "dotenv/config";
 import express from "express";
-import run from "./Controller/MessageController.js";
+// import run from "./Controller/MessageController.js";
+import AppDataSource from "./config/data-source.js";
+
 const app = express();
 app.use(express.json());
+
+// Connect DB
+AppDataSource.initialize()
+  .then(() => console.log("📌 PostgreSQL + TypeORM Connected Successfully!"))
+  .catch((err) => console.error("❌ DB Connection Error: ", err));
 
 const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running at http://localhost:${PORT}`);
 });
 
-run();
+// run();
