29ffc11a938fbda8f4022dbb344d0328a238dc62
Navi2004725 <naveeshabeydeera123@gmail.com>
2026-01-01 21:07:21 +0530

    commit after troubleshoot ✔️

--- index.js
+++ index.js
@@ -10,8 +10,4 @@ await sendSMS(
   "https://www.google.com/maps?q=6.9271,79.8612"
 );
 
-console.log("TWILIO SID:", process.env.TWILIO_ACCOUNT_SID);
-console.log("TWILIO TOKEN:", process.env.TWILIO_AUTH_TOKEN);
-console.log("TWILIO NUMBER:", process.env.TWILIO_PHONE);
-
 app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
